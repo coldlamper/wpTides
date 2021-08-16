ce093d1d4c207da8f022862dcf8b6e8c9549dc16
Brian Keith <bskeith@gmail.com>
2021-08-16 16:15:25 -0400

    Refactor array syntax

--- includes/class-wptides-loader.php
+++ includes/class-wptides-loader.php
@@ -79,11 +79,11 @@ class Wptides_Loader {
 	public function run() {
 
 		foreach ( $this->filters as $hook ) {
-			add_filter( $hook['hook'], array( $hook['component'], $hook['callback'] ), $hook['priority'], $hook['accepted_args'] );
+			add_filter( $hook['hook'], [ $hook['component'], $hook['callback'] ], $hook['priority'], $hook['accepted_args'] );
 		}
 
 		foreach ( $this->actions as $hook ) {
-			add_action( $hook['hook'], array( $hook['component'], $hook['callback'] ), $hook['priority'], $hook['accepted_args'] );
+			add_action( $hook['hook'], [ $hook['component'], $hook['callback'] ], $hook['priority'], $hook['accepted_args'] );
 		}
 
 	}
